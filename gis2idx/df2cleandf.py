6f09c79b380447ae4892c0da963612de47dc24c6
kdmcc <kdmcc@uw.edu>
2020-04-24 01:22:14 -0700

    Method for merging demographic data (works for WA)

--- gis2idx/df2cleandf.py
+++ gis2idx/df2cleandf.py
@@ -1,13 +1,15 @@
 import pickle
 import io
 import pandas as pd
+import geopandas
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
 import fiona
 import logging
 
 from util import (
-    parseState
+    parseState,
+    INPUT_CSV_LOCATION
 )
 from gis2df import (
     STEP_CACHE_LOCATION
@@ -18,6 +20,48 @@ def readLastArtifact(state: str):
     with io.open(STEP_CACHE_LOCATION + f'{state}.df.a.pk', 'rb') as handle:
         payload = pickle.loads(handle.read())
     return payload
+
+def appendDemographicsData(state: str, df):
+    demo = pd.read_csv(INPUT_CSV_LOCATION.format(state=state))
+
+    # Add the relavent columns to the main dataframe (df)
+    """
+    *******      Key to P00300X labels:      ******
+    P003001 => Total
+    P003002 => White alone
+    P003003 => Black or African American alone
+    P003004 => American Indian and Alaska Native alone
+    P003005 => Asian alone
+    P003006 => Native Hawaiian and Other Pacific Islander alone
+    P003007 => Some Other Race alone
+    P003008 => Two or More Races
+    """
+    colsToAdd = ['GEOID', 'P003001', 'P003002', 'P003003', 'P003004',
+                'P003005','P003006', 'P003007', 'P003008']
+    readableNames = ['GEOID', 'TotalPop', 'WhitePop', 'BlackPop', 'NativeAPop',
+                    'AsianPop', 'PacIsPop', 'OtherPop', 'MultiPop']
+
+    demo = demo[colsToAdd] # Narrow down demographic data to the most relevant set
+    demo = demo.rename(columns=dict(zip(colsToAdd, readableNames)))
+
+    # Merge the demographic data with the main dataframe
+    
+    # Match df's types
+    demo = demo.astype(int)
+    demo["GEOID"] = demo["GEOID"].astype(str)
+
+    # Merge the dataframes
+    df = pd.merge(df, demo, on="GEOID", how="left")
+
+    dataLeftOut = len(list(set(demo["GEOID"]) - set(df["GEOID"])))
+    if dataLeftOut > 0:
+        logging.debug(f"Unable to merge data for {dataLeftOut} precincts")
+
+    dataMissing = len(list(set(df["GEOID"]) - set(demo["GEOID"])))
+    if dataMissing > 0:
+        logging.debug(f"Unable to find data for {dataLeftOut} precincts")
+
+    return df
     
 
 def main():
@@ -42,6 +86,11 @@ def main():
     multiPoly = sum(df["geometry"].geom_type == 'MultiPolygon')
     logging.debug(f"{state} has {multiPoly} precincts represented with multipolygons")
 
+    logging.debug(f"Loading in the demographic data from {INPUT_CSV_LOCATION.format(state=state)}")
+    df = appendDemographicsData(state, df)
+    logging.debug(f"Finished merge of demographic data")
+
+    
 
 if __name__ == "__main__":
     logging.basicConfig(filename='df2cleandf.log', level=logging.DEBUG)
