762d8d56f6ab2ece9b40ec7d9cc150f15dc8e617
kdmcc <kdmcc@uw.edu>
2020-04-23 18:51:02 -0700

    Logs multipolygon and water-only precinct counts

--- gis2idx/df2cleandf.py
+++ gis2idx/df2cleandf.py
@@ -4,6 +4,7 @@ import pandas as pd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
 import fiona
+import logging
 
 from util import (
     parseState
@@ -13,19 +14,35 @@ from gis2df import (
 )
 
 def readLastArtifact(state: str):
+    "Load the previous artifact into memory"
     with io.open(STEP_CACHE_LOCATION + f'{state}.df.a.pk', 'rb') as handle:
         payload = pickle.loads(handle.read())
-    import pdb; pdb.set_trace()
     return payload
     
 
 def main():
-    state = parseState()
-    df = readLastArtifact(state)
-    #flag any water-only precincts exist
+    "Clean up the dataframes for the state, checking for edge cases that need to be handled"
 
+    # Get state
+    logging.debug(f"Parsing state")
+    state = parseState()
+    logging.debug(f"Retrieved state {state}")
+
+    logging.debug(f"Loading in the artifact: {state}.df.a.pk")
+    df = readLastArtifact(state)
+    logging.debug(f"Successfully loaded artifact")
+
+
+    logging.debug(f"Checking for edge cases")
+    #flag any water-only precincts exist
+    waterOnly = sum(df["ALAND"] == 0)
+    logging.debug(f"{state} has {waterOnly} water-only precincts")
+        
     #check for multipolygons
+    multiPoly = sum(df["geometry"].geom_type == 'MultiPolygon')
+    logging.debug(f"{state} has {multiPoly} precincts represented with multipolygons")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='df2cleandf.log', level=logging.DEBUG)
     main()
