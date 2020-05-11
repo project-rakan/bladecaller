c867fbc5c99eaa46da29209dfa8355e559c5911b
Norton Pengra <npengra317@gmail.com>
2020-05-10 19:57:18 -0700

    use python's full name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@ su - postgres -c "createdb merge_table && psql -c \"CREATE EXTENSION postgis;\""
 su - postgres -c "psql -c \"CREATE USER norton with encrypted password 'Norton00'\""
 su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE merge_table TO norton;\""
 su - postgres -c "psql -c \"ALTER USER norton WITH SUPERUSER;\""
-cd gis2idx/datamerger && py manage.py migrate && cd ../../
+cd gis2idx/datamerger && python3.7 manage.py migrate && cd ../../
